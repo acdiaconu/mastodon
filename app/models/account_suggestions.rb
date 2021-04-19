6169d8e18d2017175f4d672cbe4240939874f04a
Eugen Rochko <eugen@zeonfederated.com>
2021-04-20 01:54:15 +0200

    Change auto-following admin-selected accounts, show in recommendations

--- app/models/account_suggestions.rb
+++ app/models/account_suggestions.rb
@@ -5,8 +5,38 @@ class AccountSuggestions
     attributes :account, :source
   end
 
+  class StaffPicks
+    def self.get(account, limit)
+      return [] if Setting.bootstrap_timeline_accounts.blank?
+
+      usernames_and_domains = begin
+        Setting.bootstrap_timeline_accounts.split(',').map do |str|
+          username, domain = str.strip.gsub(/\A@/, '').split('@', 2)
+          domain           = nil if TagManager.instance.local_domain?(domain)
+
+          next if username.blank?
+
+          [username, domain]
+        end.compact
+      end
+
+      accounts = Account.searchable
+                        .followable_by(account)
+                        .not_excluded_by_account(account)
+                        .not_domain_blocked_by_account(account)
+                        .where(locked: false)
+                        .where.not(id: account.id)
+                        .where(usernames_and_domains.map { |(username, domain)| Arel::Nodes::Grouping.new(Account.arel_table[:username].lower.eq(username.downcase).and(Account.arel_table[:domain].lower.eq(domain&.downcase))) }.reduce(:or))
+                        .limit(limit)
+                        .index_by { |target_account| [target_account.username, target_account.domain] }
+
+      usernames_and_domains.map { |x| accounts[x] }.compact
+    end
+  end
+
   def self.get(account, limit)
-    suggestions = PotentialFriendshipTracker.get(account, limit).map { |target_account| Suggestion.new(account: target_account, source: :past_interaction) }
+    suggestions = StaffPicks.get(account, limit).map { |target_account| Suggestion.new(account: target_account, source: :staff) }
+    suggestions.concat(PotentialFriendshipTracker.get(account, limit - suggestions.size, suggestions.map { |suggestion| suggestion.account.id }).map { |target_account| Suggestion.new(account: target_account, source: :past_interaction) }) if suggestions.size < limit
     suggestions.concat(FollowRecommendation.get(account, limit - suggestions.size, suggestions.map { |suggestion| suggestion.account.id }).map { |target_account| Suggestion.new(account: target_account, source: :global) }) if suggestions.size < limit
     suggestions
   end
